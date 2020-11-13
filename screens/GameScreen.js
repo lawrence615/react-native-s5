898953cb4a216ada982697d9afafbc45ad8f7506
Lawrence Macharia <macharialawrence2011@gmail.com>
2020-11-13 12:47:53 +0300

    Conditional application of Dimensions API

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react'
-import { View, Text, StyleSheet, Alert, ScrollView, FlatList } from 'react-native'
+import { View, Text, StyleSheet, Alert, ScrollView, FlatList, Dimensions } from 'react-native'
 import { Ionicons } from '@expo/vector-icons'
 
 import NumberContainer from '../components/NumberContainer'
@@ -103,13 +103,15 @@ const styles = StyleSheet.create({
   buttonContainer: {
     flexDirection: 'row',
     justifyContent: 'space-around',
-    marginTop: 20,
+    // marginTop: 20,
+    marginTop: Dimensions.get('window').height > 600 ? 20 : 10,
     width: 250,
     maxWidth: '90%'
   },
   listContainer: {
     flex: 1,
-    width: '60%'
+    // width: '60%'
+    width: Dimensions.get('window').width > 350 ? '60%' : '80%'
   },
   list: {
     flexGrow: 1,
