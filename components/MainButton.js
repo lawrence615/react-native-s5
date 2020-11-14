2db5bbcb7db8f9196676af3047013bd980bbd051
Lawrence Macharia <macharialawrence2011@gmail.com>
2020-11-14 17:53:56 +0300

    improving use of Platform API

--- components/MainButton.js
+++ components/MainButton.js
@@ -1,29 +1,41 @@
 import React from 'react'
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity, TouchableNativeFeedback, Platform } from 'react-native'
 
 import Colours from '../constants/colours'
 
 const MainButton = props => {
+  let ButtonComponent = TouchableOpacity
+
+  if (Platform.OS === 'android' && Platform.Version >= 21) {
+    ButtonComponent = TouchableNativeFeedback
+  }
+
   return (
-    <TouchableOpacity activeOpacity={0.6} onPress={props.onClick}>
-      <View style={styles.button}>
-        <Text style={styles.buttonText}>{props.children}</Text>
-      </View>
-    </TouchableOpacity>
+    <View style={styles.buttonContainer}>
+      <ButtonComponent activeOpacity={0.6} onPress={props.onClick}>
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>{props.children}</Text>
+        </View>
+      </ButtonComponent>
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
+  buttonContainer: {
+    borderRadius:25,
+    overflow:'hidden'
+  },
   button: {
     backgroundColor: Colours.primary,
     paddingVertical: 12,
     paddingHorizontal: 30,
-    borderRadius:25
+    borderRadius: 25
   },
   buttonText: {
-    color:'white',
-    fontFamily:'open-sans-regular',
-    fontSize:18
+    color: 'white',
+    fontFamily: 'open-sans-regular',
+    fontSize: 18
   }
 })
 
