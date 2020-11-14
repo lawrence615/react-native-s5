5e545aa0a868b416765edd982dd34af7cbe990ba
Lawrence Macharia <macharialawrence2011@gmail.com>
2020-11-14 17:20:50 +0300

    Modified Platform check to a more elegant and code readable manner

--- components/Header.js
+++ components/Header.js
@@ -6,22 +6,27 @@ import Colours from '../constants/colours'
 
 const Header = props => {
   return (
-    <View style={styles.header}>
+    <View style={{ ...styles.headerBase, ...Platform.select({ ios: styles.headerIOS, android: styles.headerAndroid }) }}>
       <TitleText style={styles.title}>{props.title}</TitleText>
     </View >
   )
 }
 
 const styles = StyleSheet.create({
-  header: {
+  headerBase: {
     width: '100%',
     height: 90,
     paddingTop: 36,
-    backgroundColor: Platform.OS === 'android' ? Colours.primary : 'white',
     alignItems: 'center',
     justifyContent: 'center',
-    borderBottomColor: Platform.OS === 'ios' ? '#ccc' : 'transparent',
-    borderBottomWidth: Platform.OS === 'ios' ? 1 : 0
+  },
+  headerIOS: {
+    backgroundColor: 'white',
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1
+  },
+  headerAndroid: {
+    backgroundColor: Colours.primary
   },
   title: {
     color: Platform.OS === 'ios' ? Colours.primary : 'white',
