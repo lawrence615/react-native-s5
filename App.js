b9d08307d3b6ce6ca584c6d620c8f061f8252db9
Lawrence Macharia <macharialawrence2011@gmail.com>
2020-11-15 00:23:26 +0300

    Applied SafeAreaView, to render content within the safe area boundaries of a device.

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import * as Font from 'expo-font'
 import { AppLoading } from 'expo'
 
@@ -52,10 +52,10 @@ export default function App() {
   }
 
   return (
-    <View style={styles.screen}>
+    <SafeAreaView style={styles.screen}>
       <Header title="Guess a number" />
       {content}
-    </View>
+    </SafeAreaView>
   );
 }
 
